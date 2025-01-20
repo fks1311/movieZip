bc99ff3fe1771a26024ecf3cbd528fba9ac9df44
ranyoung <fks1311@gmail.com>
2025-01-21 01:24:17 +0900

    styled-components theme 적용

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,24 @@
+import Layout from "@/components/Layout";
+import theme from "@/styles/theme";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "styled-components";
 import { Reset } from "styled-reset";
 
 export default function App({ Component, pageProps }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <>
-      <Reset />
-      <Component {...pageProps} />
-    </>
+    isClient && (
+      <ThemeProvider theme={theme}>
+        <Reset />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    )
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -63,8 +63,9 @@ export default MyDocument;
  * 3. sheet.getStyleElement()를 사용하여 스타일 태그를 반환
  * 4. getInitialProps() 작업이 완료되면, render() 실행 => <Head />, <Main />, NextScript />의 HTML 구조로 반환
  * 5. * 이 과정에서 서버사이드의 Html이 올바르게 렌더링 되려면 <Head />, <Main />, NextScript /> 가 필요 => next.js 공식문서
- * 6. 추출된 styled-components의 스타일도 <head> 태그 내에 포함되어 클라이언트에게 전달
+ * 6. 추출된 styled-components의 스타일도 <head> 태그 내에 포함(style 태그)되어 클라이언트에게 전달
  * 7. 서버는 최종적으로 완성된 HTML을 클라이언트에게 전달
  * 8. 클라이언트는 HTML 파일을 전달받아 렌더링하며, 페이지가 로드되면 클라이언트 사이드에서 hydration 작업을 진행
+ *    (하이드레이션 과정에서 React는 서버에서 렌더링된 HTML을 React 컴포넌트로 변환)
  * 9. 리액트 앱이 클라이언트에서 활성화
  */
