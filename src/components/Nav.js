d588a1b87d9ea14e735c256e2e0e0c0d115f12bf
ranyoung <fks1311@gmail.com>
2025-02-04 01:24:32 +0900

    검색바 생성

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,37 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+import { AiOutlineSearch } from "react-icons/ai";
 import styled from "styled-components";
 
 export default function Nav() {
-  const nav = ["Daily", "Weekly", "Genre", "Year", "Nation"];
+  const nav = ["Genre", "Year", "Nation"];
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const handleKeydown = (e) => {};
 
   return (
     <Layout>
-      <Home>MovieZip</Home>
+      <Home
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        MovieZip
+      </Home>
       <List>
         {nav.map((nav, idx) => (
           <div key={idx}>{nav}</div>
         ))}
       </List>
+      <div>
+        <AiOutlineSearch color="white" />
+        <SearchBar
+          placeholder="검색어를 입력하세요"
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={handleKeydown}
+        />
+      </div>
     </Layout>
   );
 }
@@ -32,5 +53,20 @@ const List = styled.div`
     &:hover {
       color: ${({ theme }) => theme.color};
     }
+  }
+`;
+const SearchBar = styled(motion.input)`
+  position: absolute;
+  right: 3%;
+  border: none;
+  outline: none;
+  background: none;
+  color: white;
+  min-width: 230px;
+  padding-bottom: 5px;
+  margin-right: 1rem;
+  border-bottom: ${({ theme }) => `1px solid ${theme.grey}`};
+  &:focus {
+    border-bottom: 1px solid ${({ theme }) => theme.color};
   }
 `;
