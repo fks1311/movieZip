2cf1c574ae307bd33d3704af8f9765d58bcaccdd
ranyoung <fks1311@gmail.com>
2025-02-11 16:09:55 +0900

    uuid 라이브러리 설치 및 recoil 버그 수정

--- src/utils/atom.js
+++ src/utils/atom.js
@@ -1,12 +1,14 @@
+import { v1 } from "uuid";
+
 const { atom } = require("recoil");
 
 export const filterAtom = atom({
-  key: "filter",
+  key: `filter${v1}`,
   default: { idx: 0, filter: "전체" },
 });
 
 const getFullYear = new Date().getFullYear();
 export const yearFilterAtom = atom({
-  key: "yearFilter",
+  key: `yearFilter${v1}`,
   default: { idx: 0, filter: `${getFullYear}년` },
 });
