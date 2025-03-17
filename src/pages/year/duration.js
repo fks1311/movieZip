313be4ae0c77085444db5ca5bd63d8d03c67efb5
ranyoung <fks1311@gmail.com>
2025-03-17 13:12:37 +0900

    file 수정

--- src/pages/year/duration.js
+++ src/pages/year/duration.js
@@ -2,15 +2,17 @@ import { dateFormat } from "@/utils/date";
 
 const getFullYear = new Date().getFullYear();
 
-export const DTS = (filter) => {
+const DTS = (filter) => {
   const sel_year = Number(filter.replace(/\D/g, ""));
   return `${sel_year}0101`;
 };
 
-export const DTE = (filter) => {
+const DTE = (filter) => {
   const sel_year = Number(filter.replace(/\D/g, ""));
   const endDate = dateFormat(0);
   const duration = sel_year === getFullYear ? endDate : sel_year >= 2020 ? `${sel_year}1231` : `${sel_year + 9}1231`;
 
   return duration;
 };
+
+export { DTS, DTE };
